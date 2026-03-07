8fc50340ad4e11dd1c6f9b627dfe438fd2de0c60
Lucas <lucas@somfic.dev>
2026-03-07 22:13:22 +0100

    Delete dist directory

--- dist/cursor/CursorProvider.svelte.d.ts
+++
@@ -1,3 +0,0 @@
-declare const CursorProvider: import("svelte").Component<Record<string, never>, {}, "">;
-type CursorProvider = ReturnType<typeof CursorProvider>;
-export default CursorProvider;
--- dist/input/ColorInput.svelte.d.ts
+++
@@ -1,9 +0,0 @@
-interface Props {
-    id?: string;
-    value?: string;
-    disabled?: boolean;
-    onChange?: (value: string) => void;
-}
-declare const ColorInput: import("svelte").Component<Props, {}, "">;
-type ColorInput = ReturnType<typeof ColorInput>;
-export default ColorInput;
